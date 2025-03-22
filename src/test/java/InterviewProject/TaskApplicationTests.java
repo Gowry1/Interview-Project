4a9603b6ec81b30693ecdbc90949c680ced0217a
Gowry1 <niroshuthan24@gmail.com>
2025-03-22 11:04:25 +0530

    First Task

---
+++ src/test/java/InterviewProject/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package InterviewProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
